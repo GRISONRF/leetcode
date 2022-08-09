0c4bcaf612f2e7b440f654d3b79dd8f9972d5b8f
grisonrf <grisonrf@gmail.com>
2022-08-08 20:10:00 -0600

    top k frequent elements - NOT DONE

---
+++ top-k-frequent-elements.py
@@ -0,0 +1,55 @@
+# Given an integer array nums and an integer k, return the k most frequent elements. 
+# You may return the answer in any order.
+
+
+class Solution:
+    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+        
+        
+        # creating a dictionary - key = number, value = how many times this number appears
+        # ***** i am not sure if i can add/modify numbers to the value of a dictionary [google]
+        
+        # return the k greatest values.
+        
+        # input => [1, 1, 1, 1, 2, 2, 2, 3] k=2
+        # output => [1, 2]
+        
+        #if nums only has one number
+        if len(nums) == 1:
+            return [nums[0]]
+        
+        #if 
+        
+        
+        #creating a count for how many times n appears in nums
+        count = {} # => {1:4, 2:3, 3:1} //// {-1:2}
+
+        # iterate through nums
+        for n in nums:
+            #every time it founds n in nums, add 1 to the count[n]
+            if n in count:
+                count[n] += 1
+            else:
+                count[n] = 1
+                
+        if len(count) == 1:
+            return count.keys()
+                
+        # key = number / value = how many times
+        # return the key that has the greates value.
+        
+        #list to hold the values
+        value_holder = [] #=> [1, 2, 3]
+        #return the k greatest values in count [python dict documentation]
+        for k, v in count.items():   #=> 1:4
+            #add values to list
+            value_holder.append(k)
+            
+        # return the k highest
+        value_holder.sort() #[1, 2, 3]
+        return value_holder[:k-1]
+            
+                
+            
+        
+        
