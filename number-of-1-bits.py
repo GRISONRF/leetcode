7a3db51d1dcff0ae757a6606d4743da61bc1c3aa
grisonrf <grisonrf@gmail.com>
2022-10-20 19:56:25 -0600

    number of 1 bits

---
+++ number-of-1-bits.py
@@ -0,0 +1,10 @@
+""" Write a function that takes an unsigned integer and returns the number of '1' bits it has (also known as the Hamming weight). """
+
+def hammingWeight(n):
+    res = 0
+    while n:
+        res += n % 2
+        n = n >> 1
+    return n
+
+#not passing the test
