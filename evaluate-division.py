8a56ab836cc465e63cbc47a5d757e5b0c476b258
grisonrf <grisonrf@gmail.com>
2022-11-14 21:37:35 -0700

    399. Evaluate Division

---
+++ evaluate-division.py
@@ -0,0 +1,63 @@
+""" You are given an array of variable pairs equations and an array of real numbers values, where equations[i] = [Ai, Bi] and values[i] represent the equation Ai / Bi = values[i]. Each Ai or Bi is a string that represents a single variable.
+
+You are also given some queries, where queries[j] = [Cj, Dj] represents the jth query where you must find the answer for Cj / Dj = ?.
+
+Return the answers to all queries. If a single answer cannot be determined, return -1.0.
+
+Note: The input is always valid. You may assume that evaluating the queries will not result in division by zero and that there is no contradiction.
+
+Example 1:
+
+Input: equations = [["a","b"],["b","c"]], values = [2.0,3.0], queries = [["a","c"],["b","a"],["a","e"],["a","a"],["x","x"]]
+Output: [6.00000,0.50000,-1.00000,1.00000,-1.00000]
+Explanation: 
+Given: a / b = 2.0, b / c = 3.0
+queries are: a / c = ?, b / a = ?, a / e = ?, a / a = ?, x / x = ?
+return: [6.0, 0.5, -1.0, 1.0, -1.0 ]
+Example 2:
+
+Input: equations = [["a","b"],["b","c"],["bc","cd"]], values = [1.5,2.5,5.0], queries = [["a","c"],["c","b"],["bc","cd"],["cd","bc"]]
+Output: [3.75000,0.40000,5.00000,0.20000]
+Example 3:
+
+Input: equations = [["a","b"]], values = [0.5], queries = [["a","b"],["b","a"],["a","c"],["x","y"]]
+Output: [0.50000,2.00000,-1.00000,-1.00000]
+ """
+
+
+from collections import defaultdict
+
+
+def calcEquation(equations, values, queries):
+
+    graph = defaultdict(dict)
+    n = len(equations)
+    for i in range(n):
+        graph[equations[i][0]][equations[i][1]] = values[i]
+        graph[equations[i][1]][equations[i][0]] = 1/values[i]
+    return graph
+
+
+
+print(calcEquation([["a","b"],["b","c"]], [2.0,3.0], [["a","c"],["b","a"],["a","e"],["a","a"],["x","x"]]))
+
+
+""" 
+a -> b -> c
+
+a -> b = 2
+b -> c = 3
+
+
+a: {b: 2}
+b: {a:?, c:3}
+c: {b:?}
+
+[]
+
+
+create a dict
+for i in range(len(equations)):
+    dict[equations][i][equations][0] = value[i ]
+
+ """
