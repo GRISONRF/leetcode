b4ba1213c1394bf92555121862c3ddfb54602517
grisonrf <grisonrf@gmail.com>
2023-03-21 21:28:04 -0600

    counting bits

---
+++ 338-counting-bits.py
@@ -0,0 +1,29 @@
+class Solution:
+    def countBits(self, n: int) -> List[int]:
+
+        """ 
+        create dp
+        offset
+
+        for loop range 1, n+1
+            set the offset to the next one when we hit the *2 kind of thing
+
+            change the dp to the dp adding to the numbers that we already counted
+
+         """
+
+        dp = [0] * (n + 1)
+        offset = 1
+        
+        for i in range(1, n+1):
+            if offset * 2 == i:
+                offset = i
+
+            dp[i] = 1 + dp[i - offset]
+        
+        return dp
+
+
+             
+
+
